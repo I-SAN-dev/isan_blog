c1f57b7942582162c78befdab299561d3fb08d25
Sebastian Antosch <s.antosch@i-san.de>
2016-03-27 21:12:26 +0200

    realurl hook and nice pagination

---
+++ Classes/Hooks/RealUrlAutoConfiguration.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Isan\IsanBlog\Hooks;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+
+/**
+ * AutoConfiguration-Hook for RealURL
+ *
+ * @package TYPO3
+ * @subpackage isan_blog
+ */
+class RealUrlAutoConfiguration {
+
+    /**
+     * Generates additional RealURL configuration and merges it with provided configuration
+     *
+     * @param       array $params Default configuration
+     * @return      array Updated configuration
+     */
+    public function addIsanBlogConfig($params) {
+
+        return array_merge_recursive($params['config'], array(
+                'postVarSets' => array(
+                    '_DEFAULT' => array(
+                        'removeUnnecessary' => array(
+                            array(
+                                'GETvar' => 'tx_isanblog_blog[action]',
+                                'noMatch' => 'bypass',
+                            ),
+                            array(
+                                'GETvar' => 'tx_isanblog_blog[controller]',
+                                'noMatch' => 'bypass',
+                            ),
+                            array(
+                                'GETvar' => 'tx_isanblog_author[action]',
+                                'noMatch' => 'bypass',
+                            ),
+                            array(
+                                'GETvar' => 'tx_isanblog_author[controller]',
+                                'noMatch' => 'bypass',
+                            ),
+                        ),
+                        'author' => array(
+                            array(
+                                'GETvar' => 'tx_isanblog_blog[author]',
+                                'lookUpTable' => array(
+                                    'table' => 'tx_isanblog_domain_model_author',
+                                    'id_field' => 'uid',
+                                    'alias_field' => "name",
+                                    'useUniqueCache' => 1,
+                                    'useUniqueCache_conf' => array(
+                                        'strtolower' => 1,
+                                        'spaceCharacter' => '-',
+                                    ),
+                                ),
+                            ),
+                        ),
+                    )
+                )
+            )
+        );
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -31,3 +31,9 @@ if (!defined('TYPO3_MODE')) {
 		
 	)
 );
+
+// RealUrl autoconf
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['isan_blog'] =
+		'Isan\\IsanBlog\\Hooks\\RealUrlAutoConfiguration->addIsanBlogConfig';
+}
