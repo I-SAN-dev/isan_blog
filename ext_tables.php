07e2acb9fc5b42f44cb561d0f0c5a3f663d1d87c
Sebastian Antosch <s.antosch@i-san.de>
2016-05-17 18:15:15 +0200

    add flexform options to plugin

--- ext_tables.php
+++ ext_tables.php
@@ -3,20 +3,32 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
+// We need this!
+$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
+
+// Blog List Plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	'Isan.' . $_EXTKEY,
 	'Blog',
-	'Blog Liste und Detail'
+	'Blogpost List'
 );
+$pluginName = strtolower('Blog');
+$pluginSignature = $extensionName.'_'.$pluginName;
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive, categories';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/Blog.xml');
 
+// Author Overview Plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	'Isan.' . $_EXTKEY,
 	'Author',
 	'Author Overview'
 );
 
+// Static TS
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'I-SAN Blog');
 
+// ExtMgr Stuff
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_isanblog_domain_model_author', 'EXT:isan_blog/Resources/Private/Language/locallang_csh_tx_isanblog_domain_model_author.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_isanblog_domain_model_author');
 
